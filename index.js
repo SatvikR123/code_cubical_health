6a8e414af5f11bec7486eaa6e5c9dd94fe48ef3f
Sasuke <satvikrajput32@gmail.com>
2024-07-25 19:47:03 +0530

    added insurance cards

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.querySelector('.hamburger');
-    const navMenu = document.querySelector('.main-nav');
-
-    hamburger.addEventListener('click', function () {
-        navMenu.classList.toggle('active');
-    });
-});
